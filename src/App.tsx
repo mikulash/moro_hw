ef56d8b6aafa65f43454800e1e4e02474935fb1e
Mikulash <62067494+mikulash@users.noreply.github.com>
2024-11-25 20:11:20 +0100

    feat: load tasks on start

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.css";
-import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
+import { useAppDispatch } from "./store/store.ts";
 import { TaskList } from "@/components/TaskList.tsx";
 import { NewTaskInput } from "@/components/NewTaskInput.tsx";
+import { fetchTasks } from "@/features/tasksThunks.ts";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
   return (
-    <Provider store={store}>
-      <div className="w-full">
-        <NewTaskInput />
-        <div className="rounded-md border">
-          <TaskList />
-        </div>
+    <div className="w-full">
+      <NewTaskInput />
+      <div className="rounded-md border">
+        <TaskList />
       </div>
-    </Provider>
+    </div>
   );
 }
 
