941e5cae099db2442babcf3a5c02ed1852fa93c3
Mikulash <62067494+mikulash@users.noreply.github.com>
2024-11-28 14:56:39 +0100

    feat: column align

--- src/components/tasks/TaskTable.tsx
+++ src/components/tasks/TaskTable.tsx
@@ -69,7 +69,10 @@ export default function TaskTable({
         {table.getHeaderGroups().map((headerGroup) => (
           <TableRow key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
-              <TableHead key={header.id}>
+              <TableHead
+                key={header.id}
+                className="text-left align-middle px-4 py-2"
+              >
                 {flexRender(
                   header.column.columnDef.header,
                   header.getContext(),
@@ -83,7 +86,10 @@ export default function TaskTable({
         {table.getRowModel().rows.map((row) => (
           <TableRow key={row.id}>
             {row.getVisibleCells().map((cell) => (
-              <TableCell key={cell.id}>
+              <TableCell
+                key={cell.id}
+                className="text-left align-middle px-4 py-2"
+              >
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </TableCell>
             ))}
